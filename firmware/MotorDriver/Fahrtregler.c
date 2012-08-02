aa50da8415a2040a534143200e45df9019641523
vmoor <vladimir.moor@gmx.de>
2012-08-02 09:39:41 +0200

    add usart to motordriver

--- firmware/MotorDriver/Fahrtregler.c
+++ firmware/MotorDriver/Fahrtregler.c
@@ -7,183 +7,119 @@
 #include <util/delay.h>
 #include "2313_hard_pwm.h"
 
-#ifndef F_CPU
-#define F_CPU 4000000L
-#endif
+#define BAUD 9600
+#include <util/setbaud.h>
+
+#define UART_MAXSTRLEN 8
+
+volatile uint8_t uart_str_complete = 0;     // 1 .. String komplett empfangen
+volatile uint8_t uart_str_count = 0;
+volatile char uart_string[UART_MAXSTRLEN + 1] = "";
+
 
 
 void init_digital();         // Ports initialisieren (eigene Funktion)  
 void init_timer();           // Timer initialisieren (eigene Funktion)
 void init_interrupt();      // Interrupt initialisieren
+void USART_Init();
+;
 
-//Variablen fuer Motor 1
-volatile uint16_t start_M1;
-volatile uint16_t stop_M1;
-volatile uint16_t impuls_M1;
-volatile uint8_t flanke_M1;
+volatile int8_t max_span_for_motor = 0x00;
+volatile int8_t schritt_0_5_v = 0x00;
 
-//Variablen fuer Motor 2
-volatile uint16_t start_M2;
-volatile uint16_t stop_M2;
-volatile uint16_t impuls_M2;
-volatile uint16_t flanke_M2;
 
 int main(void)
 {
-	init_interrupt();
+
     init_digital();
 	init_timer();
 	init_pwm();	
+	USART_Init();
 
-    //PD1 und PB1 richtung von M2 pwm2  PB2 - Geschwindigkeit
-	//PD6 und PB0 richtung von M1 pwm1  PD5 - Geschwindigkeit
+	// bei Prototypen OCR0A auf OCR0B ändern
+    //PD1 und PB1 richtung von M2 pwm2  PB2(OCR0A) - Geschwindigkeit
+	//PD6 und PB0 richtung von M1 pwm1  PD5(OCR0B) - Geschwindigkeit
 	 
 
 	sei(); //Interrupts aktivieren
 	
+
+//******************************************
+		OCR0A = 0x00;
+	    PORTB |= (1<<PB1); 
+	    PORTD &= ~(1<<PD1);
+//******************************************
+	    OCR0B = 0x00;
+	    PORTB |= (1<<PB0); 
+	    PORTD &= ~(1<<PD6);
+
 	while(1)
 	{
-	//*****************************TODO test delay**********************
-	_delay_ms(1);
+	
+		if (uart_str_complete == 1) // falls der String komplett empfangen wurde
+		{
 
-	//----------- Motor 1 Steuerung START -------------  
-	  if(impuls_M1 == 0)
-	  {
-		
-	    PORTB &= ~(1<<PB1); 
-	    PORTD &= ~(1<<PD1); 
-  
-	  }
-	  if ((impuls_M1 > 445) & (impuls_M1 < 700))
-	  {
-//******************************************
-//beim Prototypen OCR0A auf OCR0B aendern
-	    OCR0A = 700 - impuls_M1;
-//******************************************
-	    PORTB |= (1<<PB1); 
-	    PORTD &= ~(1<<PD1); 
-	   }
-  
-	  if ((impuls_M1 > 800) & (impuls_M1 < 1055))
-	  {
-//******************************************
-//beim Prototypen OCR0A auf OCR0B aendern
-	    OCR0A =  impuls_M1 - 800;
-//******************************************
-	    PORTB &= ~(1<<PB1);
-	    PORTD |= (1<<PD1);
-	  }
-  
-	  if ((impuls_M1 > 700) & (impuls_M1 < 800))
-	  {
-//******************************************
-//beim Prototypen OCR0A auf OCR0B aendern
-	    OCR0A = 0x5A;
-//******************************************
-	    PORTB |= (1<<PB1); 
-	    PORTD |= (1<<PD1); 
-	  }		 
-	//----------- Motor 1 Steuerung ENDE -------------
-		
-	//----------- Motor 2 Steuerung START -------------  
-	  if(impuls_M2 == 0)
-	  {
-		
-	    PORTB &= ~(1<<PB0); 
-	    PORTD &= ~(1<<PD6); 
-  
-	  }
-	  if ((impuls_M2 > 445) & (impuls_M2 < 700))
-	  {
-//******************************************
-//beim Prototypen OCR0B auf OCR0A aendern
-	    OCR0B = 700 - impuls_M2;
-//******************************************
-	    PORTB |= (1<<PB0); 
-	    PORTD &= ~(1<<PD6); 
-	   }
-  
-	  if ((impuls_M2 > 800) & (impuls_M2 < 1055))
-	  {
-//******************************************
-//beim Prototypen OCR0B auf OCR0A aendern
-	    OCR0B =  impuls_M2 - 800;
-//******************************************
-	    PORTB &= ~(1<<PB0);
-	    PORTD |= (1<<PD6);
-	  }
-  
-	  if ((impuls_M2 > 700) & (impuls_M2 < 800))
-	  {
-//******************************************
-//beim Prototypen OCR0B auf OCR0A aendern
+			//falls motortreiber mit ID M1
+			if (uart_string[0] == 'M' && uart_string[1] == '1')
+			{
+				
+				
+				if (uart_string[6] == '0')
+				{
+					schritt_0_5_v = 255; 
+				}
+				else
 
-	    OCR0B = 0x5A;        
-//******************************************
-	    PORTB |= (1<<PB0); 
-	    PORTD |= (1<<PD6); 
-	  }		 
-	//----------- Motor 1 Steuerung ENDE -------------
+				{
+					//TODO spanunganpassung an 9 V
+					max_span_for_motor = 0x5F;
+				}
+
+				
+		
+				//Motor M2
+				//
+			//	max_span_for_motor = 0x00;
+			//	for (int8_t i = 0; i <  uart_string[3] +1 ; i++)
+				{
+//					max_span_for_motor = max_span_for_motor + schritt_0_5_v;
+				}
+
+				OCR0A = 0x00 + (max_span_for_motor / 10 * (uart_string[3] +1));
+				if(uart_string[2] == 'V')
+				{
+					 PORTB |= (1<<PB1); 
+	    			 PORTD &= ~(1<<PD1);
+				}
+				else if(uart_string[2] == 'R')
+				{
+					  PORTB &= ~(1<<PB1); 
+	   				  PORTD |= (1<<PD1);
+				}
+				
+				// Motor M1
+			    if(uart_string[2] == 'V')
+				{
+
+				}
+				else if(uart_string[2] == 'R')
+				{
+
+				}
+
+
+
+			}
+
+			uart_str_complete = 0;
+		}
 			
 	}
 }
 
-// Interrupt an INT0 auswerten
-ISR(INT0_vect)
-{
- 	cli();
- 	if (flanke_M1 == 1)
-  	{
-   		start_M1 = TCNT1;
-   		MCUCR |= (1<<ISC01); //INT0 auf fallende Flanke stellen
-   		MCUCR &= ~(1<<ISC00);
-   		flanke_M1 = 0;
-  	}
-   	else
-    {
-	 	stop_M1 = TCNT1;
-		if (stop_M1 > start_M1)
-		{
-	 		impuls_M1 = stop_M1 - start_M1;
-     	}
-		else
-		{
-			impuls_M1 = 0xFFFF - start_M1 + stop_M1;
-		}
-		MCUCR |= (1<<ISC00) | (1<<ISC01); //INT0 auf Steigende Flanke stellen (Löst Interrupt bei steigendet Flanke an INT0 aus)
-     	flanke_M1 = 1;  
-    } 
-	sei();
- 
-}
 
-// Interrupt an INT1 auswerten
-ISR(INT1_vect)
-{
- 	cli();
- 	if (flanke_M2 == 1)
-  	{
-   		start_M2 = TCNT1;
-   		MCUCR |= (1<<ISC11); //INT1 auf fallende Flanke stellen
-   		MCUCR &= ~(1<<ISC10);
-   		flanke_M2 = 0;
-  	}
-   	else
-    {
-	 	stop_M2 = TCNT1;
-	 	if (stop_M2 > start_M2)
-		{
-	 		impuls_M2 = stop_M2 - start_M2;
-     	}
-		else
-		{
-			impuls_M2 = 0xFFFF - start_M2 + stop_M2;
-		}
-     	MCUCR |= (1<<ISC10) | (1<<ISC11); //INT1 auf Steigende Flanke stellen (Löst Interrupt bei steigendet Flanke an INT1 aus)
-     	flanke_M2 = 1;  
-    } 
- 	sei();
-}
+
+
 
 void init_digital(void)
 /*Diese Funktion initialisiert die verwendeten Ports*/
@@ -195,14 +131,65 @@ void init_digital(void)
 // Timer initialisieren
 void init_timer(void)
 {
-	//Timer 1 (16Bit)
+	//Timer 1 (8Bit)
  	TCCR1B |= (1<<CS11);  //Vorteiler auf CPU Takt/8
 }
 
 
-// Interrupt auf INT0 und INT1 initialisieren
-void init_interrupt(void)
+
+
+void USART_Init()
 {
-	GIMSK  |= (1<<INT0) | (1<<INT1); //Interrupt von INT0 auf Enable
- 	MCUCR |= (1<<ISC00) | (1<<ISC01) | (1<<ISC10) | (1<<ISC11); //INT0 auf Steigende Flanke stellen (Löst Interrupt bei steigendet Flanke an INT0 aus
+
+	//Baudrate setzen
+
+	UBRRH = UBRRH_VALUE;
+    UBRRL = UBRRL_VALUE;
+		
+  	UCSRB = (1<<RXEN) | (1<<RXCIE);       // UART RX und RX Interrupt einschalten
+  	UCSRC = (3 << UCSZ0); // Asynchron 8N1 
+
+	
+
+
 }
+
+
+ISR(USART_RX_vect)
+{
+  unsigned char nextChar;
+ 
+  // Daten aus dem Puffer lesen
+  nextChar = UDR;
+
+  if(nextChar == 'n')
+  {
+	OCR0A = 0x5F;
+  }
+  
+
+
+  if( uart_str_complete == 0 ) 	// wenn uart_string gerade in Verwendung, neues Zeichen verwerfen
+  {
+    // Daten werden erst in uart_string geschrieben, 
+	//wenn nicht String-Ende/max Zeichenlänge erreicht ist/string gerade verarbeitet wird
+    if( nextChar != '\n' &&
+        nextChar != '\r' &&
+        uart_str_count < UART_MAXSTRLEN ) 
+	{
+      uart_string[uart_str_count] = nextChar;
+      uart_str_count++;
+    }
+    else 
+	{
+      uart_string[uart_str_count] = '\0';
+      uart_str_count = 0;
+      uart_str_complete = 1;
+    }
+  }
+ 
+}
+
+
+
+
