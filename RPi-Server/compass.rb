94a495a677dceab12b2f0dfcb12386daa4b757c7
vmoor <vladimir.moor@gmx.de>
2012-09-28 21:37:04 +0200

    remove unused methods

--- RPi-Server/compass.rb
+++ RPi-Server/compass.rb
@@ -41,51 +41,28 @@ class Compass
 
   end
   
-  def get_x_axis
-	return @x_axis_val  
-  end
+#  def get_x_axis
+#	return @x_axis_val  
+#  end
   
-  def get_y_axis
-	return @y_axis_val
-  end
+#  def get_y_axis
+#	return @y_axis_val
+#  end
   
-  def get_controllreg
-	return @controllreg
-  end
+#  def get_controllreg
+#	return @controllreg
+#  end
   
 #@in: value of the x-axis and y-axis from the hdmm01
 #@out:angle theta between sensor and reference-0°
   def get_inklination
-#  	/************************************************************************/
-#	/* To calculate the angle theta between sensor and reference-0° we have */
-#	/* to convert cartesian koordinates represented through the variables	*/
-#	/* x_axis and y_axis into the polar koordiantes represented	through an	*/
-#	/* uncalculated radius r and the searched angle theta.					*/
-#	/* The conversion x,y=>angle is done be the term theta = arctan(x/y).	*/
-#	/* Because of the periodicity of the tangens funtion (also the inverse	*/
-#	/* function arcustangens) we can only calculate the first 180 degrees.	*/
-#	/* The next 180 degrees we have determine through the quadrants in		*/
-#	/* the cartesian koordinates are.										*/
-#	/*																		*/
-#	/* To determine all the quadrants we have to add or subtract pi.		*/
-#	/* (I. and IV. Quadrant => normally, II. => +pi, III. => -pi)			*/
-#	/*																		*/
-#	/* SO AFTER SOME GOOGLING I HAVE FOUND OUT THAT THE STANDARD FUNCTION	*/
-#	/* atan2(x,y) DOES ALL THE STUFF NAMED IN THE UPPER PART. *fuuuuuu*		*/
-#	/*																		*/
-#	/* From the function atan2() we get back radiants so we have to convert	*/
-#	/* it back into degrees throug the function:							*/
-#	/* 360°=2pi rad  => 1rad = 360°/2pi	=> 1rad = 180°/pi					*/
-#	/*																		*/
-#	/* Not implemented is which angle represents which direction. (0° = N?) */
-#	/************************************************************************/
 
 	# conversion theta_radiants-degrees
 	@theta_degrees = Math.atan2(@x_axis_val - 2045, @y_axis_val - 2045) * 180 / PI
 	if @theta_degrees < 0
        @theta_degrees = @theta_degrees + 360	
 	end
-	return @theta_degrees
+	return @theta_degrees.round
   end
 
 end
--- RPi-Server/test_compass.rb
+++ RPi-Server/test_compass.rb
@@ -2,12 +2,10 @@
 load "compass.rb"
 
 dev = Compass.new
-puts dev.get_device
-
 while (true)
   dev.take_measurement
  # puts "control register: " + dev.get_controllreg.to_s
-  puts "x: " + dev.get_x_axis.to_s + "    y: " + dev.get_y_axis.to_s
+ #puts "x: " + dev.get_x_axis.to_s + "    y: " + dev.get_y_axis.to_s
   puts dev.get_inklination
-  sleep (0.1)
+  sleep (0.035)
 end
